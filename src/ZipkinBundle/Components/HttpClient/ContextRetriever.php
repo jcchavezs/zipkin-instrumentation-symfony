0bc47adfa7827c55f92b1b69b370c5bdb8fea45c
José Carlos Chávez <jcchavezs@gmail.com>
2020-05-07 01:18:49 +0200

    docs: adds docs for the ContextRetriever interface.

--- src/ZipkinBundle/Components/HttpClient/ContextRetriever.php
+++ src/ZipkinBundle/Components/HttpClient/ContextRetriever.php
@@ -4,6 +4,12 @@ namespace ZipkinBundle\Components\HttpClient;
 
 use Zipkin\Propagation\TraceContext;
 
+/**
+ * ContextRetriever is an API to retrieve the context from the
+ * $options array (third parameter in the HttpClient request).
+ * Implementations can use different keys from the options or
+ * just decide to use the global context in the Scope API.
+ */
 interface ContextRetriever
 {
     public function __invoke(array $options): ?TraceContext;
